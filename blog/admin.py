458e038f4a72e0b89fcab4fe8bbfa68044887d63
Lobar Badalova <lobar.fayzullayeva@mail.ru>
2020-12-20 10:43:17 +0300

    Update admin.py
    summernote bug fixed

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 from .models import Post, Category, Comments
-# Register your models here.
 
-admin.site.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+    summernote_fields = ('content',)
+
+admin.site.register(Post, PostAdmin)
 admin.site.register(Category)
 admin.site.register(Comments)
